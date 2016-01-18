186d7a67a83d47d9b0015619c5cb09fa09ab2536
Ilya Kolodnik <ikolodnik@navionics.com>
2016-01-18 13:24:11 +0300

    rails_apps_composer: extras

--- db/schema.rb
+++ db/schema.rb
@@ -26,10 +26,11 @@ ActiveRecord::Schema.define(version: 20160118102400) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
-    t.string   "name",                   limit: 255
+    t.string   "nickname",               limit: 255, default: "", null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["nickname"], name: "index_users_on_nickname", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
